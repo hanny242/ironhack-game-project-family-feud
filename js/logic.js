880beb4c82f425e05f1f13d62fbb99c2ded95c88
hanny242 <hannahdhawes@gmail.com>
2019-12-05 14:43:42 +0100

    added question logic

--- js/logic.js
+++ js/logic.js
@@ -1,53 +1,122 @@
 class FamilyFeudGame {
-    constructor(questions) {
-        this.questions = questions;
+    constructor() {
         this.players = [];
         this.currentPlayer;
         this.questionCounter = 0;
+        this.correctAnswersCounter = 0;
         //this.currentQuestion = this.questions[questionCounter];
-    }
-
-    shuffle(questions) {
-        for(let i = this.questions.length - 1; i > 0; i--){
-            const J = Math.floor(Math.random() * i)
-            const temp = this.questions[i]
-            this.questions[i] = this.questions[j]
-            this.questions[j] = temp
-        }
-        return this.question;
-    }
-
-    addPlayer(player) {
-        this.players.push(player);
-    }
-
-    checkIfRight(answer){
-        for(let i = 0; i < question.answers.length; i++){
-            if(this.questions.answers[i] === document.getElementsByClassName("playerAnswer").innerHTMl){
-                <p>${question.answers[i].answer}</p>
+        this.questions = [{
+                question: "Name a bad job for someone who's accident-prone.",
+                answers: [{
+                        answer: "Driver",
+                        percentage: 33
+                    },
+                    {
+                        answer: "Construction Worker",
+                        percentage: 20
+                    },
+                    {
+                        answer: "Police Officer",
+                        percentage: 11
+                    },
+                    {
+                        answer: "Food Service",
+                        percentage: 10
+                    },
+                    {
+                        answer: "Glass Maker",
+                        percentage: 9
+                    },
+                    {
+                        answer: "Window Washer",
+                        percentage: 7
+                    },
+                    {
+                        answer: "Surgeon",
+                        percentage: 6
+                    }
+                ]
+            },
+            {
+                question: "Name something that you hang up.",
+                answers: [{
+                        answer: "The Phone",
+                        percentage: 54
+                    },
+                    {
+                        answer: "Clothes",
+                        percentage: 38
+                    },
+                    {
+                        answer: "Towel",
+                        percentage: 4
+                    },
+                    {
+                        answer: "Picture Frame",
+                        percentage: 3
+                    }
+                ]
             }
+        ]
+
+    }
+
+
+    addPlayer() {
+        this.players.push(new Player);
+    }
+
+    checkIfRight(submittedAnswer) {
+        let answersArray = this.questions[this.questionCounter].answers
+        for (let i =0; i < answersArray.length; i++){
+            let answer = answersArray[i];
+            if (answer.answer.toLowerCase() === submittedAnswer.toLowerCase()) {
+                document.querySelector(`.answer-${i}`).innerHTML = "YES!";
+                this.correctAnswersCounter++;
+                return;
+            }
+        } 
+        this.showBigRedX()
+    }
+
+
+    showBigRedX() {
+        var bigRedX = document.querySelector(".big-red-x");
+        if (bigRedX.style.display === "none") {
+            bigRedX.style.display = "block";
+        } else {
+            bigRedX.style.display = "none";
         }
     }
 
-    nextQuestion(){
-        
+    nextQuestion() {
+        if (this.correctAnswersCounter === this.questions[this.questionCounter].answers.length){
+            this.questionCounter++;
+            this.correctAnswersCounter = 0;
+            debugger
+            renderQuestion(this.questions[this.questionCounter]);
+        }
     }
 
 
 }
 
-class PLayer {
-    constructor(){
+
+
+
+
+
+class Player {
+    constructor() {
         this.wrongAnswers = 0;
         this.rightAnswers = 0;
     }
 
     checkGameOver() {
-        if(this.questionsAnswered === questions.length) {
+        if (this.questionsAnswered === questions.length) {
             return true;
-        } else if(this.questionsAnswered < questions.length) {
+        } else if (this.questionsAnswered < questions.length) {
             return false;
         }
     }
 }
-
--- js/main.js
+++ js/main.js
@@ -1,41 +1,38 @@
 
+// const questions = [
+//     {
+//       question: "Name a bad job for someone who's accident-prone.",
+//       answers: [
+//                 {answer: "Driver", percentage: 33}, 
+//                 {answer: "Construction Worker", percentage: 20}, 
+//                 {answer: "Police Officer", percentage: 11},
+//                 {answer: "Food Service", percentage: 10}, 
+//                 {answer: "Glass Maker", percentage: 9},
+//                 {answer: "Window Washer", percentage: 7}, 
+//                 {answer: "Surgeon", percentage: 6}
+//               ]
+//     },
+//     {
+//       question: "Name something that you hang up.",
+//       answers: [
+//                 {answer: "The Phone", percentage: 54},
+//                 {answer: "Clothes", percentage: 38},
+//                 {answer: "Towel", percentage: 4},
+//                 {answer: "Picture Frame", percentage: 3}
+//       ]
+//     }
+//   ]
 
-const questions = [
-    {
-      question: "Name a bad job for someone who's accident-prone.",
-      answers: [
-                {answer: "Driver", percentage: 33}, 
-                {answer: "Construction Worker", percentage: 20}, 
-                {answer: "Police Officer", percentage: 11},
-                {answer: "Food Service", percentage: 10}, 
-                {answer: "Glass Maker", percentage: 9},
-                {answer: "Window Washer", percentage: 7}, 
-                {answer: "Surgeon", percentage: 6}
-              ]
-    },
-    {
-      question: "Name something that you hang up.",
-      answers: [
-                {answer: "The Phone", percentage: 54},
-                {answer: "Clothes", percentage: 38},
-                {answer: "Towel", percentage: 4},
-                {answer: "Picture Frame", percentage: 3}
-      ]
-    }
-  ]
+let game = new FamilyFeudGame()
 
-let theGame = new FamilyFeudGame(questions);
-
-
-
-window.addEventListener("load", event => {
-  renderQuestion(theGame.questions[0])
+window.addEventListener("load", () => {
+  renderQuestion(game.questions[0])
 });
 
 function renderQuestion(question){
     let answersHTML = "";
     for(let i = 0; i < question.answers.length; i++) {
-       answersHTML += `<p>${question.answers[i].answer}</p>`
+       answersHTML += `<p class="answer-${i}">${question.answers[i].answer}</p>`
     }
     document.getElementsByClassName("question")[0].innerHTML = `
       <div class="question">
@@ -45,14 +42,18 @@ function renderQuestion(question){
     `;
 }
 
-function revealAnswers(question){
+document.querySelector(".submit-answer").addEventListener("click", function(){
+  let answer = document.querySelector('.playerAnswer').value
+  game.checkIfRight(answer);
+  game.nextQuestion();
+})
 
-}
 
-document.querySelector(".answer").onclick = function() {
+
+//document.querySelector(".submit-answer").onclick = function() {
   // todo B
   // get value
   // check if answer is correct
   // go to next question
   // render next question
-}
+//}
